5c0a4a4dcdc777dd417aeb875a8a2ec401869a30
tomas <tomas.ew88@gmail.com>
2025-11-28 08:55:38 +0200

    Fix argument parser logic

--- ECommerceBackend.Utils/Src/Database/PostgresConfigurationBuilder.cs
+++ ECommerceBackend.Utils/Src/Database/PostgresConfigurationBuilder.cs
@@ -4,29 +4,22 @@ public class PostgresConfigurationBuilder
 {
     public PostgresConfiguration Build(string[] args)
     {
-        string? host = args.FirstOrDefault(arg => arg.StartsWith("host"))?.Split()?[1];
-        string? database = args.FirstOrDefault(arg => arg.StartsWith("database"))?.Split()?[1];
-        string? username = args.FirstOrDefault(arg => arg.StartsWith("username"))?.Split()?[1];
-        string? password = args.FirstOrDefault(arg => arg.StartsWith("password"))?.Split()?[1];
-        string? port = args.FirstOrDefault(arg => arg.StartsWith("port"))?.Split()?[1];
-
-        int? portNum;
-        if (port != null)
-        {
-            if (!int.TryParse(port, out portNum))
-            {
-                throw new ArgumentException("Port must be an integer!");
-            }
-        }
-        else
-        {
-            portNum = 5432;
-        }
+        string? host = args.FirstOrDefault(arg => arg.StartsWith("--host"))?.Split("=")?[1];
+        string? database = args.FirstOrDefault(arg => arg.StartsWith("--database"))?.Split("=")?[1];
+        string? username = args.FirstOrDefault(arg => arg.StartsWith("--username"))?.Split("=")?[1];
+        string? password = args.FirstOrDefault(arg => arg.StartsWith("--password"))?.Split("=")?[1];
+        string port = args.FirstOrDefault(arg => arg.StartsWith("--port"))?.Split("=")?[1] ?? "5432";
         
         ArgumentException.ThrowIfNullOrWhiteSpace(host);
         ArgumentException.ThrowIfNullOrWhiteSpace(database);
         ArgumentException.ThrowIfNullOrWhiteSpace(username);
         ArgumentException.ThrowIfNullOrWhiteSpace(password);
+        
+        int portNum;
+        if (!int.TryParse(port, out portNum))
+        {
+            throw new ArgumentException("Port must be an integer!");
+        }
 
         return new PostgresConfiguration
         {
