388924f623a5c88d5fbfd62ee7b3a6f70693d9e0
Tomas <tomas.ew88@gmail.com>
2025-12-04 15:18:31 +0200

    Jwt package for reading JWT config and reading JWT contents from HttpContext

---
+++ ECommerceBackend.Utils.Jwt/Src/Jwt.cs
@@ -0,0 +1,32 @@
+namespace ECommerceBackend.Utils.Jwt;
+
+public class JwtAuthConfiguration
+{
+    public required string Issuer { get; set; }
+    public required string Audience { get; set; }
+    public required string SigningKey { get; set; }
+    public required int LifetimeMinutes { get; set; }
+}
+
+public static class ActorTypes
+{
+    public static string Client { get; } = "client";
+    public static string Microservice { get; } = "microservice";
+}
+
+public abstract class JwtClaims
+{
+    public required string Issuer { get; init; }
+    public required string Actor { get; init; }
+}
+
+public class ClientJwtClaims : JwtClaims
+{
+    public required string UserId { get; init; }
+    public required string Email { get; init; }
+}
+
+public class MicroserviceJwtClaims : JwtClaims
+{
+    public string[] Permissions { get; set; } = [];
+}
