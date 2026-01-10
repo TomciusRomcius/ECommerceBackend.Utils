f6ebbd5ccb0252f6fba2fb352a2a23ba60b78c39
Tomas <tomas.ew88@gmail.com>
2026-01-10 18:21:25 +0200

    Remove Src from namespace

--- ECommerceBackend.Utils.Auth/Src/AuthSetup.cs
+++ ECommerceBackend.Utils.Auth/Src/AuthSetup.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace ECommerceBackend.Utils.Auth.Src;
+namespace ECommerceBackend.Utils.Auth;
 
 public static class AuthSetup
 {
