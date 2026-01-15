e169cc9571bef5090218be52676783675c407690
Tomas <tomas.ew88@gmail.com>
2026-01-15 18:59:52 +0200

    EventTypes package for publishing and consuming kafka events in microservices

---
+++ ECommerceBackend.EventTypes/EventTypes.cs
@@ -0,0 +1,12 @@
+﻿namespace ECommerceBackend.EventTypes;
+
+public class ChargeSucceededEvent
+{
+    public required string UserId { get; set; }
+    public required int Amount { get; set; }
+}
+
+public class UserAccountDeleted
+{
+    public required string UserId { get; set; }
+}
