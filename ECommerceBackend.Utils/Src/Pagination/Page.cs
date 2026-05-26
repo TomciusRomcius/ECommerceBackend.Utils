c7b5a1baa89ccd0b0b80980c5f301e4a5e5f5b78
Tomas Rimkevicius <tomas.ew88@gmail.com>
2026-05-26 17:42:11 +0300

    Page include PageSize

--- ECommerceBackend.Utils/Src/Pagination/Page.cs
+++ ECommerceBackend.Utils/Src/Pagination/Page.cs
@@ -6,4 +6,5 @@ public class Page<T>
     public bool HasNextPage { get; init; }
     public bool HasPrevPage { get; init; }
     public int TotalCount { get; init; }
+    public int PageSize { get; init; }
 }
