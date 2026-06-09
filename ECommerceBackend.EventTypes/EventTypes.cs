5d9020e997ad2cc48564162dd7dfab7dbf06acc7
Tomas Rimkevicius <tomas.ew88@gmail.com>
2026-06-09 12:57:25 +0300

    Add additional event types

--- ECommerceBackend.EventTypes/EventTypes.cs
+++ ECommerceBackend.EventTypes/EventTypes.cs
@@ -1,19 +1,86 @@
 ﻿namespace ECommerceBackend.EventTypes;
 
-public class ChargeSucceededEvent
+public abstract class Event
+{
+    public Guid MessageId { get; set; } = Guid.NewGuid();
+}
+
+public class ChargeSucceededEvent : Event
 {
     public required string UserId { get; set; }
     public required string OrderId { get; set; }
     public required int Amount { get; set; }
 }
 
-public class PaymentFailedEvent
+// Product service
+public class ProductCreatedEvent : Event
+{
+    public required int ProductId { get; set; }
+    public required string Name { get; set; }
+    public required decimal Price { get; set; }
+}
+
+public class ProductUpdatedEvent : Event
+{
+    public required int ProductId { get; set; }
+    public required string Name { get; set; }
+    public required decimal Price { get; set; }
+}
+
+
+public class ProductDeletedEvent : Event
+{
+    public required int ProductId { get; set; }
+}
+
+// Store service
+public class ProductAddedToStoreEvent : Event
+{
+    public required int StoreLocationId { get; set; }
+    public required int ProductId { get; set; }
+    public required int Stock { get; set; }
+}
+
+public class ProductStockUpdatedEvent : Event
+{
+    public required int StoreLocationId { get; set; }
+    public required int ProductId { get; set; }
+    public required int Stock { get; set; }
+}
+
+public class ProductRemovedFromStoreEvent : Event
+{
+    public required int StoreLocationId { get; set; }
+    public required int ProductId { get; set; }
+}
+
+public class StoreCreatedEvent : Event
+{
+    public required int StoreLocationId { get; set; }
+    public required string DisplayName { get; set; }
+    public required string Address { get; set; }
+}
+
+public class StoreUpdatedEvent : Event
+{
+    public required int StoreLocationId { get; set; }
+    public required string DisplayName { get; set; }
+    public required string Address { get; set; }
+}
+
+public class StoreDeletedEvent : Event
+{
+    public required int StoreLocationId { get; set; }
+}
+
+public class PaymentFailedEvent : Event
 {
     public required string UserId { get; set; }
     public required string OrderId { get; set; }
 }
 
-public class UserAccountDeleted
+public class UserAccountDeleted : Event
 {
     public required string UserId { get; set; }
 }
+
