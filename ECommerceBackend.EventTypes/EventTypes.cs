6e08d221c18c58a0a55da53603991dc4caffa118
tomas <tomas.ew88@gmail.com>
2026-06-14 17:23:59 +0300

    Populate topic names inside event types

--- ECommerceBackend.EventTypes/EventTypes.cs
+++ ECommerceBackend.EventTypes/EventTypes.cs
@@ -3,7 +3,7 @@
 public abstract class Event
 {
     public Guid MessageId { get; } = Guid.NewGuid();
-    public required string TopicName { get; init; }
+    public abstract string TopicName { get; }
 }
 
 public class ChargeSucceededEvent : Event
@@ -11,6 +11,7 @@ public class ChargeSucceededEvent : Event
     public required string UserId { get; set; }
     public required string OrderId { get; set; }
     public required int Amount { get; set; }
+    public override string TopicName => "charge_succeeded";
 }
 
 // Product service
@@ -19,6 +20,7 @@ public class ProductCreatedEvent : Event
     public required int ProductId { get; set; }
     public required string Name { get; set; }
     public required decimal Price { get; set; }
+    public override string TopicName => "product_created";
 }
 
 public class ProductUpdatedEvent : Event
@@ -26,12 +28,14 @@ public class ProductUpdatedEvent : Event
     public required int ProductId { get; set; }
     public required string Name { get; set; }
     public required decimal Price { get; set; }
+    public override string TopicName => "product_updated";
 }
 
 
 public class ProductDeletedEvent : Event
 {
     public required int ProductId { get; set; }
+    public override string TopicName => "product_deleted";
 }
 
 // Store service
@@ -40,6 +44,7 @@ public class ProductAddedToStoreEvent : Event
     public required int StoreLocationId { get; set; }
     public required int ProductId { get; set; }
     public required int Stock { get; set; }
+    public override string TopicName => "product_added_to_store";
 }
 
 public class ProductStockUpdatedEvent : Event
@@ -47,12 +52,14 @@ public class ProductStockUpdatedEvent : Event
     public required int StoreLocationId { get; set; }
     public required int ProductId { get; set; }
     public required int Stock { get; set; }
+    public override string TopicName => "product_stock_updated";
 }
 
 public class ProductRemovedFromStoreEvent : Event
 {
     public required int StoreLocationId { get; set; }
     public required int ProductId { get; set; }
+    public override string TopicName => "product_removed_from_store";
 }
 
 public class StoreCreatedEvent : Event
@@ -60,6 +67,7 @@ public class StoreCreatedEvent : Event
     public required int StoreLocationId { get; set; }
     public required string DisplayName { get; set; }
     public required string Address { get; set; }
+    public override string TopicName => "store_created";
 }
 
 public class StoreUpdatedEvent : Event
@@ -67,54 +75,64 @@ public class StoreUpdatedEvent : Event
     public required int StoreLocationId { get; set; }
     public required string DisplayName { get; set; }
     public required string Address { get; set; }
+    public override string TopicName => "store_updated";
 }
 
 public class StoreDeletedEvent : Event
 {
     public required int StoreLocationId { get; set; }
+    public override string TopicName => "store_deleted";
 }
 
 public class PaymentFailedEvent : Event
 {
     public required string UserId { get; set; }
     public required string OrderId { get; set; }
+    public override string TopicName => "payment_failed";
 }
 
 public class UserAccountDeleted : Event
 {
     public required string UserId { get; set; }
+    public override string TopicName => "user_account_deleted";
 }
 
 public class ManufacturerCreatedEvent : Event
 {
     public required int ManufacturerId { get; set; }
     public required string Name { get; set; }
+    public override string TopicName => "manufacturer_created";
 }
 
 public class ManufacturerUpdatedEvent : Event
 {
     public required int ManufacturerId { get; set; }
     public required string Name { get; set; }
+    public override string TopicName => "manufacturer_updated";
 }
 
 public class ManufacturerDeletedEvent : Event
 {
     public required int ManufacturerId { get; set; }
+    public override string TopicName => "manufacturer_deleted";
 }
 
 public class CategoryCreatedEvent : Event
 {
     public required int CategoryId { get; set; }
     public required string Name { get; set; }
+    public override string TopicName => "category_created";
 }
 
 public class CategoryUpdatedEvent : Event
 {
     public required int CategoryId { get; set; }
     public required string Name { get; set; }
+    public override string TopicName => "category_updated";
 }
 
 public class CategoryDeletedEvent : Event
 {
     public required int CategoryId { get; set; }
+    public override string TopicName => "category_deleted";
 }
