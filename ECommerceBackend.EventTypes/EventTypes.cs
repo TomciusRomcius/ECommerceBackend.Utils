8ff24f370e0ffc0ddb381395bd124f307cb90cde
Tomas Rimkevicius <tomas.ew88@gmail.com>
2026-06-17 16:28:09 +0300

    Product image events

--- ECommerceBackend.EventTypes/EventTypes.cs
+++ ECommerceBackend.EventTypes/EventTypes.cs
@@ -142,3 +142,26 @@ public class CategoryDeletedEvent : Event
     public required int CategoryId { get; set; }
     public override string TopicName => "category_deleted";
 }
+
+public class ProductImageCreatedEvent : Event
+{
+    public required int ProductImageId { get; set; }
+    public required int ProductId { get; set; }
+    public required string S3Key { get; set; }
+    public override string TopicName => "product_image_created";
+}
+
+public class ProductImageUpdatedEvent : Event
+{
+    public required int ProductImageId { get; set; }
+    public required int ProductId { get; set; }
+    public required string S3Key { get; set; }
+    public override string TopicName => "product_image_updated";
+}
+
+public class ProductImageDeletedEvent : Event
+{
+    public required int ProductImageId { get; set; }
+    public required int ProductId { get; set; }
+    public override string TopicName => "product_image_deleted";
+}
